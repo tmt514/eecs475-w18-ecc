5f2bea62ca3f3cc3682b9fcb8b5d0f8144cad2bf
tmt514 <math.tmt514@gmail.com>
2018-03-18 11:09:45 -0400

    update testing flow

--- src/app.js
+++ src/app.js
@@ -13,8 +13,10 @@ import {Server} from 'hapi';
 import {
   PORT,
   CMD_COMPILE_F,
+  CMD_RUN_F,
   DIRNAME_F,
   DIRPATH_F,
+  PREPROCESS_CODE,
 } from './secrets'; 
 
 /**
@@ -45,7 +47,7 @@ const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 const OK = 1
 const FAIL = 0
-const create_temporary_directory = async (socket, code, dirPath) => {
+const create_temporary_directory = async (socket, code, dirPath, mainFile) => {
   socket.emit('judge_msg', 'creating temporary directory!')
 
   socket.emit('judge_msg', 'copying files...')
@@ -57,6 +59,8 @@ const create_temporary_directory = async (socket, code, dirPath) => {
   fs.copySync(path.resolve(__dirname, '../files/uberzahl.cpp'), dirPath + '/uberzahl.cpp')
   fs.copySync(path.resolve(__dirname, '../files/ec_ops.h'), dirPath + '/ec_ops.h')
   fs.copySync(path.resolve(__dirname, '../files/uberzahl.h'), dirPath + '/uberzahl.h')
+  fs.copySync(path.resolve(__dirname, `../files/${mainFile}`), dirPath + '/main.cpp')
+
   socket.emit('judge_msg', 'copying files... OK!')
 
 }
@@ -84,7 +88,25 @@ const compile = async (socket, dirPath) => {
 
 const runtest = async (socket, dirPath) => {
 
-  socket.emit('judge_msg', '\nSome tests will be added before due... please check this site later :)')
+  socket.emit('judge_msg', '\nRunning your code...')
+  
+  const cmd = CMD_RUN_F(dirPath)
+
+  try {
+    const { stdout, stderr } = await exec(cmd)
+    const output = fs.readFileSync(dirPath + '/RuntimeLog.txt').toString()
+    socket.emit('judge_msg', 'Running your code... OK!\n\n')
+    socket.emit('judge_msg', output.substr(0, 4096))
+  } catch (e) {
+		if (e.code == 124) {
+      socket.emit('judge_msg', 'Timed out! (60 seconds)')
+		} else if (e.stdout.length > 0) {
+      socket.emit('judge_msg', e.stdout.substr(0, 1024))
+    }
+    throw Error('Runtime Error!')
+  }
+
+  socket.emit('judge_msg', '\nSome more tests will be added before due... please check this site later :)')
 
 }
 
@@ -96,13 +118,18 @@ var seq = 0
 io.on('connection', (socket) => {
  
   socket.on('submit', async (code) => {
+
+    // grader issue: replace main function
+    code = PREPROCESS_CODE(code)
+
+    
     const today = new Date()
     seq += 1
     const dirname = DIRNAME_F(today, seq)   
     const dirPath = DIRPATH_F(dirname) 
     console.log(`creating temp dir ${dirPath}`)
     try {
-      await create_temporary_directory(socket, code, dirPath)
+      await create_temporary_directory(socket, code, dirPath, 'main.cpp')
       await compile(socket, dirPath)
       await runtest(socket, dirPath)
     } catch (e) {
